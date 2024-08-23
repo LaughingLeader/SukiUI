9e4fe10046fb18ccf1a8274a52b0781fbeb18ed6
LaughingLeader <laughingleader@gmail.com>
2024-08-23 17:12:36 -0500

    Add a converter for StrokeWidth to Margin

---
+++ SukiUI/Converters/DoubleThicknessConverter.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+
+using System;
+using System.Globalization;
+
+namespace SukiUI.Converters
+{
+    public class DoubleThicknessConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is double v)
+            {
+                return new Thickness(v);
+            }
+            return new Thickness();
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if(value is Thickness v)
+            {
+                return v.Left;
+            }
+            return 0;
+        }
+    }
+}
